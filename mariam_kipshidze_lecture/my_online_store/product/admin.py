52ba6c6e8cf2e463370f2ac3036fe7e2d48e3cf3
Mariam <mari.kifshidze@gmail.com>
2024-09-05 20:34:13 +0400

    one to one connection

--- mariam_kipshidze_lecture/my_online_store/product/admin.py
+++ mariam_kipshidze_lecture/my_online_store/product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from product.models import Product, Category
+from product.models import Product, Category, Cart
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Cart)
