707375d00e04e5664ae76015181b464d3e265d20
Mariam <mari.kifshidze@gmail.com>
2024-09-05 21:24:11 +0400

    M2M connection

--- mariam_kipshidze_lecture/my_online_store/product/models/__init__.py
+++ mariam_kipshidze_lecture/my_online_store/product/models/__init__.py
@@ -2,3 +2,4 @@ from .product import *
 from .category import *
 from .cart import *
 from .brand import *
+from .tag import *
--- mariam_kipshidze_lecture/my_online_store/product/models/product.py
+++ mariam_kipshidze_lecture/my_online_store/product/models/product.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-
-
 class Product(models.Model):
     brand = models.ForeignKey(
         to='product.Brand',
@@ -11,12 +9,19 @@ class Product(models.Model):
         on_delete=models.PROTECT,
         null=True
     )
+    category = models.ManyToManyField(
+        to='product.Category',
+        verbose_name=_("Category"),
+        blank=True
+    )
+    tag = models.ManyToManyField(
+        to='product.Tag',
+        verbose_name=_("Tag"),
+        blank=True
+    )
     title = models.CharField(verbose_name=_('Title'), max_length=255)
     price = models.DecimalField(verbose_name=_('Price'), max_digits=5, decimal_places=2)
     description = models.TextField(verbose_name=_('Description'), blank=True, null=True)
-    category = models.SmallIntegerField(
-        verbose_name=_('Category'),
-        choices=((1, 'category_1'), (2, 'category_2'), (3, 'category_3'), (4, 'category_4')))
     active = models.BooleanField(verbose_name=_('Active'), default=True)
     create = models.DateTimeField(verbose_name=_('Created'), auto_now_add=True)
     updated = models.DateTimeField(verbose_name=_('Updated'), auto_now=True)
